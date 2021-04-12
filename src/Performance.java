4d41adda5bbd06fc2b465ca2b46c8f39cbb0443c
murat <muratcelik11@gmail.com>
2021-04-12 03:59:12 +0300

    via

--- src/Performance.java
+++ src/Performance.java
@@ -9,9 +9,9 @@ public class Performance {
 
     public static void main(String[] args) {
 
-//		ArrayList<Integer> li = new ArrayList<Integer>();   //10000-11000 civarý "listele" //ekle 3400-4000 //sil 25000 //güncelle 15000-16000
-//		List<Integer> li = new ArrayList<Integer>();        //11000-12000 civarý "listele" //ekle 3800-4200 //sil 25000 //güncelle 17000-18000
-        LinkedList<Integer> li = new LinkedList<Integer>(); //26000-27000 civarý "listele" //ekle 4300-4700 //sil 31400 //güncelle 28000
+//		ArrayList<Integer> li = new ArrayList<Integer>();   //10000-11000  "listele" //ekle 3400-4000 //sil 25000 //güncelle 15000-16000
+//		List<Integer> li = new ArrayList<Integer>();        //11000-12000  "listele" //ekle 3800-4200 //sil 25000 //güncelle 17000-18000
+        LinkedList<Integer> li = new LinkedList<Integer>(); //26000-27000  "listele" //ekle 4300-4700 //sil 31400 //güncelle 28000
         for (int i = 0; i < 100; i++) {
             li.add(i);
         }
